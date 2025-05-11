2bea22e2b4ec72ece9552e6d762e45d0a3393377
VinodKumarKP <31899217+VinodKumarKP@users.noreply.github.com>
2025-05-11 15:41:21 -0600

    Create hello2.py

---
+++ hello2.py
@@ -0,0 +1,13 @@
+# Example of Python code with code style issues
+
+def addNumbers( a,b):return a+b
+def subtractNumbers(a , b ):
+ return a - b
+def multiplyNumbers(a,b): return a*b
+ def divideNumbers(a,b): 
+  if b != 0: return a / b
+  else: return None
+print(addNumbers(5,3)) 
+print(subtractNumbers(5,3)) 
+print(multiplyNumbers(5,3)) 
+print(divideNumbers(5,0))
